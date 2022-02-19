b8a8df443eb3379ef764c8c1c99c5b86ff251ef2
souviksaha35 <webintersect123@gmail.com>
2022-02-19 12:58:53 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/lead-module'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/lead-module'})
+})
+
+app.listen(process.env.PORT || 5000);
